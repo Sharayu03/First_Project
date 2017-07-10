31ceecec14a02992a2610a2bb3783ac047a59bd8
sharayu <sharayuhemant.fukey@accoliteindia.com>
2017-07-10 23:41:34 +0530

    Code Done
    Signed-off-by: sharayu <sharayuhemant.fukey@accoliteindia.com>

---
+++ src/Class_One.java
@@ -0,0 +1,20 @@
+
+
+import javax.swing.plaf.metal.MetalIconFactory.FolderIcon16;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class Class_One {
+	int m = 0;
+
+	public static void main(String args[]) {
+		final int a = 1;
+		System.out.println("This is first java program");
+
+		int arr[] = { 1, 2, 3, 4, 5 };
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
